e31341b6af630906aaf3aac859e6293b1cbf660d
ranjith1807 <v.ranjith9095490240@gmail.com>
2025-12-18 18:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function firstWord(s) {
   // your code here
 	let str=s.trim();
-	str=s.split(" ")
-	return str[0];
+	let str1=str.split(" ")
+	return str1[0];
 }
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
 
 
